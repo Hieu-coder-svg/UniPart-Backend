a69a22a5d7d4a7a957f1622e97acb10d69c5fa63
Nguyen Minh Hieu <nguyenhieutd2k4@gmail.com>
2026-04-11 14:12:12 +0700

    add OTP

---
+++ src/main/java/com/unipart/unipart_backend/entity/Otp.java
@@ -0,0 +1,21 @@
+package com.unipart.unipart_backend.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class OTPRequest {
+    int otp;
+    String email;
+    LocalDateTime expirationTime;
+}
