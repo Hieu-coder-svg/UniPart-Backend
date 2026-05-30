5981be6475540632f56440e31032db9b42f1ea14
Nguyen Minh Hieu <nguyenhieutd2k4@gmail.com>
2026-05-30 08:38:15 +0700

    add web url

--- src/main/java/com/unipart/unipart_backend/configuration/WebSocketConfig.java
+++ src/main/java/com/unipart/unipart_backend/configuration/WebSocketConfig.java
@@ -26,7 +26,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:5173", "http://localhost:*")
+                .setAllowedOriginPatterns("http://localhost:5173", "http://localhost:*", "https://unipart.vercel.app", "https://*.vercel.app")
                 .addInterceptors(webSocketAuthInterceptor)
                 .withSockJS();
     }
